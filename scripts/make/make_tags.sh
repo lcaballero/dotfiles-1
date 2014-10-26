4c5a9d730371fb29764a27db1b2eefa5148ed154
Kartik Shenoy <theresmorethanoneway@gmail.com>
2014-10-26 09:37:31 -0600

    Moved readmes

--- scripts/make/make_tags.sh
+++ scripts/make/make_tags.sh
@@ -5,15 +5,21 @@ if [ -z $REPO_PATH ]; then
 fi
 
 # directories to search
-TAG_DIRS=($REPO_PATH/libs/verif $REPO_PATH/ch $REPO_PATH/import/avf)
-# clean up temp files
-rm -f $REPO_PATH/tags.filelist
-rm -f $REPO_PATH/tags
-rm -f $REPO_PATH/cscope.out
-# find files
-find ${TAG_DIRS[@]} -type f \( -iname "*.h" -or -iname "*.cc" \) > $REPO_PATH/tags.filelist
-# follow symlinks for generates files
+TAG_DIRS=($REPO_PATH/libs/verif $REPO_PATH/ch)
+export TAGS_DB=$REPO_PATH/.tags
+export CSCOPE_DB=$REPO_PATH/.cscope.out
+
+# Clean up temp files
+rm -f $TAGS_DB
+rm -f $CSCOPE_DB
+
+# Find files
+find ${TAG_DIRS[@]} -type f \( -iname "*.h" -or -iname "*.cc" \) >| $REPO_PATH/tags.filelist
+# Follow symlinks for generates files
 find ${TAG_DIRS[@]} -type l \( -iname "*.h" -or -iname "*.cc" \) -exec readlink '{}' \; >> $REPO_PATH/tags.filelist
-/tool/pandora64/latest/bin/ctags --languages=c++ --c++-kinds=+p --fields=+Sail --extra=+q -L $REPO_PATH/tags.filelist -f $REPO_PATH/tags &
-cscope -b -i $REPO_PATH/tags.filelist -f $REPO_PATH/cscope.out &
+
+/tool/pandora64/latest/bin/ctags --languages=c++ --c++-kinds=+p --fields=+Sail --extra=+q -L $REPO_PATH/tags.filelist -f $TAGS_DB 2> /dev/null &
+cscope -bqk -i $REPO_PATH/tags.filelist -f $CSCOPE_DB 2> /dev/null &
+
 wait
+rm -f $REPO_PATH/tags.filelist
